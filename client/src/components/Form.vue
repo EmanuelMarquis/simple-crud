50f9d3ce4f83c4ab4b528ddf0c536fd1a01243b3
Emanuel Marquis <eman0dev@gmail.com>
2023-06-15 10:09:48 -0300

    feat/crud-operations

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,13 +1,64 @@
 <script setup>
-import { ref } from 'vue'
+import axios from 'axios'
+import { ref, onBeforeMount } from 'vue'
+
+let cubes = ref("...")
+
+onBeforeMount(async () => {
+  cubes.value = (await axios.get("/api/cubes")).data
+})
+
 </script>
 
-<template><div>
-  <form @submit="(e) => {e.preventDefault();}" method="post">
-    <input type="text" maxlength="32" name="msg"/>
-    <input type="submit"  value="create"/>
-  </form>
-</div></template>
+<template>
+<div class="form">
+  <input v-model="msg" type="text" placeholder="message" maxlength="32"/>
+  <input v-model="color" type="color"/>
+  <input type="button" value="create" @click="() => createCube(msg, color, async ()=> cubes=await getCubes())"/>
+  <input type="button" value="delete all" @click="() => deleteAllCubes(async () => cubes = await getCubes())"/>
+</div>
+<div class="form">
+  <input v-model="id" type="number" min="1" placeholder="id"/>
+  <input type="button" value="delete" @click="() => deleteCube(id, async () => cubes = await getCubes())"/>
+  <input type="button" value="get cube" @click="async () => cubes = await getCube(id)"/>
+  <input type="button" value="replace" @click="() => editCube(id, msg, color, async () => cubes = await getCubes())"/>
+</div>
+<span>{{ cubes }}</span>
+</template>
+
+<script>
+export default {
+  data: () => {return{
+    msg: '',
+    color: '#ffffff',
+    id: 1
+  }},
+  methods: {
+    createCube(msg, color, onCreate) {
+      axios.post("/api/cubes", {msg: msg, color: color})
+        .then((res) => onCreate()) // on success
+    },
+    async getCubes() {
+      return (await axios.get("/api/cubes")).data
+    },
+    async getCube(id) {
+      return (await axios.get(`/api/cubes/${id}`)).data
+    },
+    editCube(id, msg, color, onEdit) {
+      axios.put(`/api/cubes/${id}`, {msg: msg, color: color})
+        .then((res) => onEdit())
+    },
+    deleteCube(id, onDeletion) {
+      axios.delete(`/api/cubes/${id}`)
+        .then((res) => onDeletion())
+    },
+    deleteAllCubes(onDeletion) {
+      axios.delete("/api/cubes")
+        .then((res) => onDeletion()) // on success
+    }
+  }
+}
+</script>
 
 <style scoped>
 input {
@@ -19,7 +70,7 @@ input[type="text"] {
   border: solid;
 }
 
-form {
+.form {
   height: fit-content;
   display: flex;
   gap: 1rem;
