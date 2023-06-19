ef3a45a298861e4adf6edcc4fed4f5620f90228a
Emanuel Marquis <eman0dev@gmail.com>
2023-06-19 14:02:57 -0300

    refact/dashboard-and-create-views

--- client/src/views/Create.vue
+++ client/src/views/Create.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { createCube } from '../services/consumer';
+
+</script>
+
 <template>
-    <p>Here u create cubes</p>
+    <p>Here u create cubes:</p>
+    <span>
+        <input v-model="msg" type="text" placeholder="messgae" maxlength="32"/>
+        <input v-model="color" type="color"/>
+        <RouterLink to="/"><input type="button" value="create" @click="createCube(msg, color)"/></RouterLink>
+    </span>
 </template>
+
+<script>
+export default {
+    data: () => {return {
+        msg: '',
+        color: '#ffffff'
+    }}
+}
+</script>
+
+<style scoped>
+span {
+    display: flex;
+    gap: 1rem;
+}
+</style>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,3 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import { getCubes } from '../services/consumer';
+import { onBeforeMount } from 'vue';
+import { RouterLink } from 'vue-router';
+
+let cubes = ref({})
+
+onBeforeMount(async () => {
+    cubes.value = await getCubes()
+})
+</script>
+
 <template>
-    <p>Here you see the existing cubes</p>
+    <div>
+        <p>Here you see the existing cubes:</p>
+        <p>{{ cubes }}</p>
+        <RouterLink to="/create"><input type="button" value="new cube" /></RouterLink>
+    </div>
 </template>
