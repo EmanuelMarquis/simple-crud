f35925c55a09fb260d2dc3dc06861c14ac4246b1
Emanuel Marquis <eman0dev@gmail.com>
2023-06-22 11:56:24 -0300

    feat/route-links

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,30 +1,23 @@
 <script setup>
-import { ref } from 'vue';
-import { getCubes } from '../services/consumer';
+import { getCubes, getData } from '../services/consumer';
 import { onBeforeMount } from 'vue';
-import { RouterLink, useRouter } from 'vue-router';
+import { RouterLink } from 'vue-router';
 import Cube from '../components/Cube.vue';
 
-let cubes = ref({})
-
-useRouter().afterEach(async (to, from) => {
-    if(!(to.fullPath === "/")) return
-    cubes.value = await getCubes()
-})
-
 onBeforeMount(async () => {
-    cubes.value = await getCubes()
+    getData().cubes.value = await getCubes()
 })
 </script>
 
 <template>
     <div>
         <p>Here you see the existing cubes:</p>
-        <p>{{ cubes }}</p>
-        <ul v-for="cube in cubes">
-            <Cube :color="cube.color" :msg="cube.msg" />
+        <p>{{ getData().cubes }}</p>
+        <ul v-for="cube in getData().cubes">
+            <RouterLink :to="'/cube/' + cube.id">
+                <Cube :color="cube.color" :msg="cube.msg"/>
+            </RouterLink>
         </ul>
-        
         <RouterLink to="/create"><input type="button" value="new cube" /></RouterLink>
     </div>
 </template>
