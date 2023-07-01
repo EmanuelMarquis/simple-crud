3487dac9b89109da4869a7e7555e4966c3a9ef15
Emanuel Marquis <eman0dev@gmail.com>
2023-07-01 12:03:04 -0300

    feat: created popover

---
+++ client/src/components/Popover.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+defineProps({
+    text: String,
+    actionText: String,
+    action: Function,
+    cancelAction: Function
+})
+
+</script>
+<template>
+    <div>
+        <p>{{ text }}</p>
+        <span>
+            <input type="button" value="cancel" @click="cancelAction"/>
+            <input id="delete" type="button" :value="actionText" @click="action"/>
+        </span>
+    </div>
+</template>
+<style scoped>
+
+span {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+}
+
+p {
+    text-align: center;
+}
+
+#delete {
+    background-color: var(--delete-color);
+}
+
+input {
+    padding: 0.5rem;
+    font-size: inherit;
+    border-radius: 0.5rem;
+}
+
+</style>
